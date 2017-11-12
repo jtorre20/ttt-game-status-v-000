a70ec8d76f2b5982d9921545170ab01c4852216b
Julio Torres <jtorres1989@gmail.com>
2017-11-12 13:58:07 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,7 +61,11 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || draw?(board) == true || (full?(board) == true || full?(board) == false)
+  if won?(board) == true && full?(board) == true 
+    true
+  elsif won?(board) == true && full?(board) == false
+    true
+  elsif draw?(board) == true 
     true
   elsif full?(board) == false
     false
