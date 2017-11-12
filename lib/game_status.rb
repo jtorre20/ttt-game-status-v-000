f121c988d24f1e84fbfde0d5d2e053ee4a00f93c
Julio Torres <jtorres1989@gmail.com>
2017-11-12 16:55:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -77,9 +77,6 @@ def winner(board)
   value = won?(board)
      board[value[0]]
 
-     if won?(board) == false
-       return nil
-     end
     #|| board[3]#board[0] == board[1] && board[1] == board[2]
 
 
