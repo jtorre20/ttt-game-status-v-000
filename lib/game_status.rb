572168141add643f889a127a482a4fc34feeafe2
Julio Torres <jtorres1989@gmail.com>
2017-11-12 13:49:05 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -61,7 +61,9 @@ def draw?(board)
 end
 
 def over?(board)
-  if won?(board) == true || (draw?(board) == true || draw?(board) == false) || (full?(board) == true || full?(board) == false)
+  if won?(board) == true || draw?(board) == true || (full?(board) == true || full?(board) == false)
     true
+  else 
+    
 end
 end
